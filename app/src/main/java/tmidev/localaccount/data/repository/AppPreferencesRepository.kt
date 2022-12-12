01e4328079fa310974617cce7bbc9829cb8e6ddb
Thales Isidoro <tminet@live.com>
2022-12-12 09:52:18 -0300

    added documentation for app preferences repository

--- app/src/main/java/tmidev/localaccount/data/repository/AppPreferencesRepository.kt
+++ app/src/main/java/tmidev/localaccount/data/repository/AppPreferencesRepository.kt
@@ -6,20 +6,53 @@ import tmidev.localaccount.domain.model.AppConfiguration
 import tmidev.localaccount.domain.model.type.ThemeStyle
 import javax.inject.Inject
 
+/**
+ * Repository interface for application preferences/configurations.
+ */
 interface AppPreferencesRepository {
+    /**
+     * Data stream of essential preferences.
+     */
     val appConfigurationStream: Flow<AppConfiguration>
 
+    /**
+     * Toggles between using dynamic colors or not.
+     */
     suspend fun toggleDynamicColors()
 
+    /**
+     * Change the app theme.
+     *
+     * @param themeStyle the [ThemeStyle] to be used.
+     */
     suspend fun changeThemeStyle(themeStyle: ThemeStyle)
 
+    /**
+     * Checks if the user chooses to stay signed in.
+     *
+     * @return true if should reconnect, false otherwise.
+     */
     suspend fun isStayConnectedEnabled(): Boolean
 
+    /**
+     * Set the current user id.
+     *
+     * @param userId [Int] value that represents the user id to be saved.
+     * @param stayConnected [Boolean] value to indicate when this user should be reconnected.
+     */
     suspend fun setCurrentUserId(userId: Int, stayConnected: Boolean)
 
+    /**
+     * Get the current user id.
+     *
+     * @return [Int] value from current user id or 0 if there is no id saved.
+     */
     suspend fun getCurrentUserId(): Int
 }
 
+/**
+ * Implementation of [AppPreferencesRepository] that uses [LocalAccountPreferences].
+ */
 class AppPreferencesRepositoryImpl @Inject constructor(
     private val localAccountPreferences: LocalAccountPreferences
 ) : AppPreferencesRepository {
