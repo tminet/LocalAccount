912b0cc67cdc92e1247205aabb9a31b76e81ab91
Thales Isidoro <tminet@live.com>
2023-03-24 11:00:31 -0300

    updated typography style

--- app/src/main/java/tmidev/localaccount/ui/component/theme/Typography.kt
+++ app/src/main/java/tmidev/localaccount/ui/component/theme/Typography.kt
@@ -8,7 +8,7 @@ import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
 import tmidev.localaccount.R
 
-private val QuickSandFontFamily = FontFamily(
+private val quickSandFontFamily = FontFamily(
     Font(resId = R.font.quicksand_normal, weight = FontWeight.Normal),
     Font(resId = R.font.quicksand_medium, weight = FontWeight.Medium),
     Font(resId = R.font.quicksand_bold, weight = FontWeight.Bold)
@@ -19,101 +19,108 @@ private val QuickSandFontFamily = FontFamily(
  */
 val LaTypography = Typography(
     displayLarge = TextStyle(
-        fontFamily = QuickSandFontFamily,
+        fontFamily = quickSandFontFamily,
         fontWeight = FontWeight.Normal,
         fontSize = 57.sp,
         lineHeight = 64.sp,
         letterSpacing = (-0.25).sp
     ),
     displayMedium = TextStyle(
-        fontFamily = QuickSandFontFamily,
+        fontFamily = quickSandFontFamily,
         fontWeight = FontWeight.Normal,
         fontSize = 45.sp,
-        lineHeight = 52.sp
+        lineHeight = 52.sp,
+        letterSpacing = 0.sp
     ),
     displaySmall = TextStyle(
-        fontFamily = QuickSandFontFamily,
+        fontFamily = quickSandFontFamily,
         fontWeight = FontWeight.Normal,
         fontSize = 36.sp,
-        lineHeight = 44.sp
+        lineHeight = 44.sp,
+        letterSpacing = 0.sp
     ),
     headlineLarge = TextStyle(
-        fontFamily = QuickSandFontFamily,
+        fontFamily = quickSandFontFamily,
         fontWeight = FontWeight.Normal,
         fontSize = 32.sp,
-        lineHeight = 40.sp
+        lineHeight = 40.sp,
+        letterSpacing = 0.sp
     ),
     headlineMedium = TextStyle(
-        fontFamily = QuickSandFontFamily,
+        fontFamily = quickSandFontFamily,
         fontWeight = FontWeight.Normal,
         fontSize = 28.sp,
-        lineHeight = 36.sp
+        lineHeight = 36.sp,
+        letterSpacing = 0.sp
     ),
     headlineSmall = TextStyle(
-        fontFamily = QuickSandFontFamily,
+        fontFamily = quickSandFontFamily,
         fontWeight = FontWeight.Normal,
         fontSize = 24.sp,
-        lineHeight = 32.sp
+        lineHeight = 32.sp,
+        letterSpacing = 0.sp
     ),
     titleLarge = TextStyle(
-        fontFamily = QuickSandFontFamily,
+        fontFamily = quickSandFontFamily,
         fontWeight = FontWeight.Bold,
         fontSize = 22.sp,
-        lineHeight = 28.sp
+        lineHeight = 28.sp,
+        letterSpacing = 0.sp
     ),
     titleMedium = TextStyle(
-        fontFamily = QuickSandFontFamily,
+        fontFamily = quickSandFontFamily,
         fontWeight = FontWeight.Bold,
-        fontSize = 16.sp,
+        fontSize = 18.sp,
         lineHeight = 24.sp,
         letterSpacing = 0.1.sp
     ),
     titleSmall = TextStyle(
-        fontFamily = QuickSandFontFamily,
+        fontFamily = quickSandFontFamily,
         fontWeight = FontWeight.Medium,
         fontSize = 14.sp,
         lineHeight = 20.sp,
         letterSpacing = 0.1.sp
     ),
     bodyLarge = TextStyle(
-        fontFamily = QuickSandFontFamily,
+        fontFamily = quickSandFontFamily,
         fontWeight = FontWeight.Normal,
         fontSize = 16.sp,
         lineHeight = 24.sp,
         letterSpacing = 0.5.sp
     ),
     bodyMedium = TextStyle(
-        fontFamily = QuickSandFontFamily,
+        fontFamily = quickSandFontFamily,
         fontWeight = FontWeight.Normal,
         fontSize = 14.sp,
         lineHeight = 20.sp,
         letterSpacing = 0.25.sp
     ),
     bodySmall = TextStyle(
-        fontFamily = QuickSandFontFamily,
+        fontFamily = quickSandFontFamily,
         fontWeight = FontWeight.Normal,
         fontSize = 12.sp,
         lineHeight = 16.sp,
         letterSpacing = 0.4.sp
     ),
     labelLarge = TextStyle(
-        fontFamily = QuickSandFontFamily,
-        fontWeight = FontWeight.Normal,
+        fontFamily = quickSandFontFamily,
+        fontWeight = FontWeight.Medium,
         fontSize = 14.sp,
         lineHeight = 20.sp,
         letterSpacing = 0.1.sp
     ),
     labelMedium = TextStyle(
-        fontFamily = QuickSandFontFamily,
-        fontWeight = FontWeight.Normal,
+        fontFamily = quickSandFontFamily,
+        fontWeight = FontWeight.Medium,
         fontSize = 12.sp,
         lineHeight = 16.sp,
         letterSpacing = 0.5.sp
     ),
     labelSmall = TextStyle(
-        fontFamily = FontFamily.Monospace,
+        fontFamily = quickSandFontFamily,
         fontWeight = FontWeight.Medium,
         fontSize = 10.sp,
-        lineHeight = 16.sp
+        lineHeight = 16.sp,
+        letterSpacing = 0.sp
     )
 )
